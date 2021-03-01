406bfd24d536e214751b326421fc4d399383e23a
Harshad Tahiliani <tharshad50@gmail.com>
2021-03-01 19:04:49 +0530

    LomL

---
+++ app/src/main/java/com/example/database/SQLiteHelper.java
@@ -0,0 +1,109 @@
+package com.example.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+
+//    Defining table and columns
+    public static final String TABLE_NAME = "user";
+    public static final String id = "id";
+    public static final String name = "name";
+    public static final String password = "password";
+
+    static final String DB_NAME = "demo";
+    static final int DB_VERSION = 1;
+
+    private static final String CREATE_TABLE = "CREATE TABLE "+TABLE_NAME + "(" + id + " INTEGER PRIMARY KEY AUTOINCREMENT , " + name + " VARCHAR, " + password + " VARCHAR)";
+
+
+    public SQLiteHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+//        Creating table
+        db.execSQL(CREATE_TABLE);
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+//        Drop table if exists
+        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        onCreate(db);
+    }
+
+//    Adding User into database
+    public boolean addUser(UserInfo userInfo) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues userVal = new ContentValues();
+        userVal.put(name, userInfo.getName());
+        userVal.put(password, userInfo.getPassword());
+
+        if(db.insert(TABLE_NAME, null ,userVal) == -1) {
+
+            db.close();
+            return false;
+        }
+        else {
+
+            db.close();
+            return true;
+        }
+    }
+
+//    Select one row from database where id = userId
+    public Cursor userRow(int userId) {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.rawQuery("SELECT * FROM "+ TABLE_NAME + " WHERE id = "+userId, null);
+        return cursor;
+    }
+
+//        delete one row from database where id = userId
+    public boolean userDelete(int userId) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        if( db.delete(TABLE_NAME, " id = "+userId, null) == 1 ) {
+
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+//        Update one row from database where id = userId
+    public boolean userUpdate(int userId, String userName, String userPassword) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues userUpdateVal = new ContentValues();
+        userUpdateVal.put("Name", userName);
+        userUpdateVal.put("Password", userPassword);
+        if( db.update(TABLE_NAME, userUpdateVal, "id = "+userId, null) > 0) {
+            db.close();
+            return true;
+        }
+        else {
+            db.close();
+            return false;
+        }
+    }
+
+//    Selecting all rows from database
+    public Cursor userAllSelect () {
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM "+TABLE_NAME, null);
+        return cursor;
+    }
+}
---
+++ app/src/main/java/com/example/database/UserInfo.java
@@ -0,0 +1,34 @@
+package com.example.database;
+
+public class UserInfo {
+
+    private int id;
+    private String name;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public UserInfo(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
